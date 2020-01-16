307dc0e7b22e02c71ebf694d428449bf8bf34c00
XiaoRGEEK <service@xiaorgeek.com>
2020-01-16 19:02:26 +0800

    202001161902

--- enums.d.ts
+++ enums.d.ts
@@ -26,40 +26,26 @@
 
     declare const enum RemoteButton
     {
-    //% block=One
-    One = 69,
-    //% block=Two
-    Two = 70,
-    //% block=Three
-    Three = 71,
-    //% block=Four
-    Four = 68,
-    //% block=Five
-    Five = 64,
-    //% block=Six
-    Six = 67,
-    //% block=Seven
-    Seven = 7,
-    //% block=Eight
-    Eight = 21,
-    //% block=Nine
-    Nine = 9,
-    //% block=Zero
-    Zero = 25,
-    //% block=Asterisk
-    Asterisk = 22,
-    //% block=Sharp
-    Sharp = 13,
-    //% block=Up
-    Up = 24,
-    //% block=Down
-    Down = 82,
-    //% block=Left
-    Left = 8,
-    //% block=Right
-    Right = 90,
-    //% block=Ok
-    Ok = 28,
+    Power = 0x45,
+    Menu = 0x47,
+    Test = 0x44,
+    Plus = 0x40,
+    Return = 0x43,
+    Left = 0x07,
+    Play = 0x15,
+    Right = 0x09,
+    Num0 = 0x16,
+    Minus = 0x19,
+    Cancle = 0x0D,
+    Num1 = 0x0C,
+    Num2 = 0x18,
+    Num3 = 0x5E,
+    Num4 = 0x08,
+    Num5 = 0x1C,
+    Num6 = 0x5A,
+    Num7 = 0x42,
+    Num8 = 0x52,
+    Num9 = 0x4A,
     }
 declare namespace Explorer_IR {
 }
--- explorerIR.cpp
+++ explorerIR.cpp
@@ -7,63 +7,49 @@ using namespace pxt;
 typedef vector<Action> vA;
 
 enum class Pins{
-  P0=  3,
-  P1=  2,
-  P2=  1,
-  P3=  4,
-  P4=  5,
-  P5=  17,
-  P6=  12,
-  P7=  11,
-  P8=  18,
-  P9=  10,
-  P10= 6,
-  P11= 26,
-  P12= 20,
-  P13= 23,
-  P14= 22,
-  P15= 21,
-  P16= 16,
-  P19= 0,
-  P20= 30
+  P0 = 3,
+  P1 = 2,
+  P2 = 1,
+  P3 = 4,
+  P4 = 5,
+  P5 = 17,
+  P6 = 12,
+  P7 = 11,
+  P8 = 18,
+  P9 = 10,
+  P10 = 6,
+  P11 = 26,
+  P12 = 20,
+  P13 = 23,
+  P14 = 22,
+  P15 = 21,
+  P16 = 16,
+  P19 = 0,
+  P20 = 30,
 };
 
 enum class RemoteButton
 {
-  //% block=One
-  One = 69,
-  //% block=Two
-  Two = 70,
-  //% block=Three
-  Three = 71,
-  //% block=Four
-  Four = 68,
-  //% block=Five
-  Five = 64,
-  //% block=Six 
-  Six = 67,
-  //% block=Seven  
-  Seven = 7,  
-  //% block=Eight    
-  Eight = 21,
-  //% block=Nine    
-  Nine = 9,
-  //% block=Zero    
-  Zero = 25,
-  //% block=Asterisk    
-  Asterisk = 22,
-  //% block=Sharp    
-  Sharp = 13,
-  //% block=Up    
-  Up = 24,
-  //% block=Down    
-  Down = 82,
-  //% block=Left    
-  Left = 8,
-  //% block=Right    
-  Right = 90,
-  //% block=Ok    
-  Ok = 28
+  Power = 0x45,
+  Menu = 0x47,
+  Test = 0x44,
+  Plus = 0x40,
+  Return = 0x43,
+  Left = 0x07,
+  Play = 0x15,
+  Right = 0x09,
+  Num0 = 0x16,
+  Minus = 0x19,
+  Cancle = 0x0D,
+  Num1 = 0x0C,
+  Num2 = 0x18,
+  Num3 = 0x5E,
+  Num4 = 0x08,
+  Num5 = 0x1C,
+  Num6 = 0x5A,
+  Num7 = 0x42,
+  Num8 = 0x52,
+  Num9 = 0x4A,
 };
 
 //% color=#5b78fa weight=10 
